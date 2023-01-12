5532bd33f1fb1e157d0ae89ff3bb05a8797ae437
Taraansh <taraanshnandwani123@gmail.com>
2023-01-12 12:49:58 +0530

    Created a new component TextForm, used states and events

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
@@ -7,6 +8,9 @@ function App() {
     <Navbar title="TextUtils" />
     {/*<Navbar title="TextUtils" aboutText="About TextUtils" />*/}
     {/*<Navbar/>*/}
+    <div className="container my-3">
+    <TextForm heading="Enter the text below to analyze:"/>
+    </div>
     </>
   );
 }
