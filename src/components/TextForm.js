16d51f6d7dd533ea962a6cbb32ecff3f8f56f16e
Taraansh <taraanshnandwani123@gmail.com>
2023-01-12 17:54:57 +0530

    created remove extra space button and copy button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -22,6 +22,17 @@ export default function TextForm(props) {
         setText(event.target.value)
     }
 
+    const handleCopy = () =>{
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = () =>{
+        let newText = text.split(/[  ]+/);
+        setText(newText.join(" "))
+    }
+
     const [text, setText] = useState('Enter text here');
     // text = "new text"; //Wrong way to change state
     // setText("new text"); //correct way to change state
@@ -34,6 +45,8 @@ export default function TextForm(props) {
         </div>
         <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to uppercase</button>
         <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to lowercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         <button className="btn btn-primary mx-1" onClick={handleClear}>Clear Text</button>
     </div>
     <div className="container my-3">
