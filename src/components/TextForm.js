d3cc10ca8e5890249b49c27df089fc3e5dbc68b6
Taraansh <taraanshnandwani123@gmail.com>
2023-01-12 13:18:46 +0530

    Created preview and a lowercase button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,6 +7,12 @@ export default function TextForm(props) {
         setText(newText);
     }
     
+    const handleLowClick = () =>{
+        //console.log("Uppercase was clicked");
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    
     const handleOnChange = (event) =>{
         //console.log("On change");
         setText(event.target.value)
@@ -16,12 +22,22 @@ export default function TextForm(props) {
     // text = "new text"; //Wrong way to change state
     // setText("new text"); //correct way to change state
     return (
-    <div>
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+        <button className="btn btn-primary" onClick={handleLowClick}>Convert to lowercase</button>
     </div>
+    <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
   )
 }
