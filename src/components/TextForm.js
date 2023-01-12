35d07ca4ffa057c69c89fa4a73391e6bae8ef856
Taraansh <taraanshnandwani123@gmail.com>
2023-01-12 13:45:57 +0530

    Made some modifications

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -28,11 +28,11 @@ export default function TextForm(props) {
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
-        <button className="btn btn-primary" onClick={handleLowClick}>Convert to lowercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to lowercase</button>
     </div>
     <div className="container my-3">
-        <h1>Your text summary</h1>
+        <h2>Your text summary</h2>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
