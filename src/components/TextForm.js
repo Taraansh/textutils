57a9258bf61781ed122abe0efff8440f2d03a60c
Taraansh <taraanshnandwani123@gmail.com>
2023-01-20 20:27:43 +0530

    Modified view for phones

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -48,15 +48,15 @@ export default function TextForm(props) {
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'grey':'white', color: props.mode === 'dark'?'white':'#010c0f'}} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to uppercase</button>
-        <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to lowercase</button>
-        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-        <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
-        <button className="btn btn-primary mx-1" onClick={handleClear}>Clear Text</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to uppercase</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleLowClick}>Convert to lowercase</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleClear}>Clear Text</button>
     </div>
     <div className="container my-3" style={{color: props.mode === 'dark'?'white':'#010c0f'}}>
         <h2>Your text summary</h2>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:"Enter something to preview"}</p>
